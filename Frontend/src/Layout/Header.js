b8192b00575b1b6adae8fdd707674e94b8e1228b
PC C <pc_chua@outlook.com>
2022-03-28 22:32:38 +0800

    Add logo to UI and browser tab

--- Frontend/src/Layout/Header.js
+++ Frontend/src/Layout/Header.js
@@ -2,10 +2,15 @@ import React from "react";
 import classes from "./Header.module.css";
 import bgImage from "../assets/background_image.jpg";
 import { NavLink } from "react-router-dom";
+import logo from '../logo-white.png'
 const Header = () => {
   return (
     <React.Fragment>
       <header className={classes.header}>
+        <img
+          src={logo}
+          className="d-inline-block align-top"
+        />
         <h1
           style={{
             marginLeft: "50px",
--- Frontend/src/Utils/Constants.js
+++ Frontend/src/Utils/Constants.js
@@ -1,4 +1,5 @@
 export const Constants = {
-  api_endpoint: "http://165.173.19.80:5050",
+  // api_endpoint: "http://165.173.19.80:5050",
   // api_endpoint: "https://weekends-do-what.herokuapp.com",
+  api_endpoint: "http://127.0.0.1:5000"
 };
