7ac23efde780c0c6251a4631f213852f629ca49c
PC C <pc_chua@outlook.com>
2022-04-03 15:56:44 +0800

    Improve how ads look

--- Frontend/src/components/Itineraries.js
+++ Frontend/src/components/Itineraries.js
@@ -5,6 +5,13 @@ import useFullPageSpinner from "../hooks/useFullPageSpinner";
 import Card from "../Layout/Card";
 import { Constants } from "../Utils/Constants";
 import Activities from "./Activities";
+import classes from "./Itineraries.module.css";
+
+// Images for fake ads
+import buffet from "../assets/buffet.jpg";
+import genius from "../assets/genius.jpg";
+import whois from "../assets/whois.jpg";
+
 const Itineraries = (props) => {
   const [locationList, setLocationList] = useState([]);
 
@@ -47,6 +54,7 @@ const Itineraries = (props) => {
   return (
     <Fragment>
       {loader}
+      <div>
       {locationList.map((loc, index) => {
         return (
           <Card>
@@ -62,6 +70,36 @@ const Itineraries = (props) => {
           </Card>
         );
       })}
+      </div>
+
+      {/* Fake ads */}
+      <div className={classes.ads}>
+        <div>
+          <script data-num-rows="3" src="https://s3-us-west-2.amazonaws.com/ads/ads.js" type="text/javascript"></script>
+          <div className={classes.adsModule}>
+            <div className="adsItems">
+              {/* <a href="http://www.fool.com/video-alert/stock-advisor/sa-nightmare-gfx/"> */}
+              <div className={classes.adsItem}>
+                  <img src={buffet} />
+                  <p>4 in 5 Americans Are Ignoring Buffet's Warning</p>
+                </div>
+              {/* </a> */}
+              {/* <a href="http://elitedaily.com/life/satisfied-people-dont-wait-want-go-get"> */}
+                <div className={classes.adsItem}>
+                  <img src={genius} />
+                  <p>10 Tips To Learn Any Language From The Genius Who Speaks 9</p>
+                </div>
+              {/* </a> */}
+              {/* <a href="http://elitedaily.com/humor/who-is-this-text-most-insulting-text-video/"> */}
+              <div className={classes.adsItem}>
+                  <img src={whois} />
+                  <p>Why 'Who Is This?' Is Literally The Most Insulting Test Ever (Video)</p>
+                </div>
+              {/* </a> */}
+            </div>
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 };
